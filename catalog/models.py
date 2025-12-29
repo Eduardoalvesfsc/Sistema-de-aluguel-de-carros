81f021dfc4295e17860337ea6ffabce6202fd0b4
Eduardo Ferreira <eduardoalvesferreirafsc@gmail.com>
2025-12-29 18:15:59 -0300

    Definido modelo carro e configurado aplicações no settings.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    placa = models.CharField(max_length=10, unique=True)
+    modelo = models.CharField(max_length=30)
+    marca = models.CharField(max_length=100)
+    ano = models.CharField(max_length=4)
+    total_modelos = models.PositiveIntegerField(default=1)
+    total_disponivel = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.total_disponivel = self.total_modelos
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.modelo

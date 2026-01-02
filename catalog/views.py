81c109bd20b6fb83638b5d549fdc84c034f3f162
Eduardo Ferreira <eduardoalvesferreirafsc@gmail.com>
2026-01-02 15:37:18 -0300

    Criando views catalogo

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Carro
+from .forms import CarroForm
 
-# Create your views here.
+def carro_list(request):
+    carros = Carro.objects.all()
+    return render(request, 'catalog/carro_list.html', {'carros': carros})
+
+def add_carro(request):
+    if request.method =='POST':
+        form = CarroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('carro_list')
+    else:
+        form = CarroForm()
+    return render(request, 'catalog/add_carro.html', {'form': form})

2a5d500019936f4c4c8f8b46bd6cc7fbed54345c
Eduardo Ferreira <eduardoalvesferreirafsc@gmail.com>
2026-01-06 17:11:12 -0300

    Criando views emprestimo

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import carro_alugado
+from .forms import CarroAlugadoForm
+from catalog.models import Carro
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def carro_alugado(request):
+    if request.method == 'POST':
+        form = CarroAlugadoForm(request.POST)
+        if form.is_valid():
+            alugado = form.save(commit=False)
+            alugado.user = request.user
+            alugado.save()
+
+            # Diminuir das copias disponiveis
+            carro = alugado.carro
+            carro.total_disponivel -= 1
+            carro.save()
+
+            return redirect('meus_carros')
+    else:
+        form = CarroAlugadoForm()
+    return render(request, 'borrowing/carro_alugado.html', {'form': form})

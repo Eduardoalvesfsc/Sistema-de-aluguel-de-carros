e3d0fb7408a542b76354e7c04bb7ac9b263133ea
Eduardo Ferreira <eduardoalvesferreirafsc@gmail.com>
2026-04-04 01:28:07 -0300

    Ajuste de UX, mensagem de indisponibilidade de veiculo

--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Carro
 from .forms import CarroForm
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib import messages
 
 def carro_list(request):
     carros = Carro.objects.all()
@@ -20,3 +21,16 @@ def add_carro(request):
 def is_funcionario(user):
     return user.is_staff
 
+def carro_alugado(request, carro_id):
+    carro = get_object_or_404(Carro, id=carro_id)
+
+    if carro.total_disponivel <= 0:
+        messages.error(request, "Carro indisponível no momento.")
+        return redirect('carro_list')
+
+    # lógica de aluguel
+    carro.total_disponivel -= 1
+    carro.save()
+
+    messages.success(request, "Carro alugado com sucesso!")
+    return redirect('meus_carros')
